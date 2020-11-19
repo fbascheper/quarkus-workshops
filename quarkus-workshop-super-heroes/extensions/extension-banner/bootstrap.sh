0741a70f3c139158b6dc1184a0376f75e890c510
Erik-Berndt Scheper <erik.berndt.scheper@gmail.com>
2020-11-19 12:01:14 +0100

    Upgrade to Quarkus 1.9.2 and Java 11

--- quarkus-workshop-super-heroes/extensions/extension-banner/bootstrap.sh
+++ quarkus-workshop-super-heroes/extensions/extension-banner/bootstrap.sh
@@ -35,8 +35,8 @@ echo "<project xmlns='http://maven.apache.org/POM/4.0.0' xmlns:xsi='http://www.w
     <properties>
         <quarkus.version>1.7.0.Final</quarkus.version>
         <surefire-plugin.version>2.22.0</surefire-plugin.version>
-        <maven.compiler.source>1.8</maven.compiler.source>
-        <maven.compiler.target>1.8</maven.compiler.target>
+        <maven.compiler.source>11</maven.compiler.source>
+        <maven.compiler.target>11</maven.compiler.target>
         <maven.build.timestamp.format>yyyy-MM-dd</maven.build.timestamp.format>
         <project.reporting.outputEncoding>UTF-8</project.reporting.outputEncoding>
         <project.build.sourceEncoding>UTF-8</project.build.sourceEncoding>
